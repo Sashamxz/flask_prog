80f81a359bd7660e71629d96767c54485de948cd
Sashamxz <sasha.oom7@gmail.com>
2021-04-19 10:53:22 -0700

    создание шаблона

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/help ')
+def helper():
+    return 'this is help page'
+
+if __name__=='__main__':
+    app.debug = True 
+    app.run()    
