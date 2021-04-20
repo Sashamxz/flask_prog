c7ba54cf76e1aff2e258dd1ed1ae3f785a72403f
Sashamxz <sasha.oom7@gmail.com>
2021-04-20 10:40:21 -0700

    work

--- app.py
+++ app.py
@@ -5,11 +5,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    print(i)
     return 'Hello, World!'
 
-@app.route('/help ')
+@app.route('/help')
 def helper():
     return 'this is help page'
+
+@app.route('/user/<int:id>/')
+def user_profile(id):
+    return "Profile page of user #{}".format(id)
+
+
+
+
 
 if __name__=='__main__':
     app.debug = True 
