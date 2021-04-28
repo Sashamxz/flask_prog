d75ca97c6845ac4f96421fe2f101695fb2071efe
Sashamxz <sasha.oom7@gmail.com>
2021-04-28 13:40:12 +0000

    new_db

--- app.py
+++ app.py
@@ -1,26 +1,19 @@
 from flask import Flask, request, render_template
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.debug = True
+manager = Manager(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db= SQLAlchemy
+
 
 
 @app.route('/')
 def index():
     return render_template('index.html', name='Admin')
-
-
-@app.route('/login/', methods=['post', 'get'])
-def login():
-    message = ''
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-    if username == 'root' and password == 'pass':
-        message = "Correct username and password"
-    else:
-        message = "Wrong username or password"
-
-    return render_template('login.html', message=message)
 
 
 @app.route('/help')
@@ -33,6 +26,5 @@ def user_profile(id):
     return "Profile page of user #{}".format(id)
 
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
+if __name__=='__main__':
+    manager.run()
