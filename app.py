4da40da2f743e665dbe74ba3e8c1fcd35d55a4d2
Sashamxz <sasha.oom7@gmail.com>
2021-06-15 12:54:19 +0000

    CRM

--- app.py
+++ app.py
@@ -7,8 +7,31 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://postgres:123@localhost/test.db'
 db = SQLAlchemy(app)
+app.config.from_object(__name__)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1024), nullable=False)
+ 
+    def __init__(self, text,tags):
+        self.text = text.stript()
+        self.tags = [
+           Tag(text=tag.strip()) for tag in tags.split(',')
+        ]
+     
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(32), nullable=False)
+    
+    message_id = db.Column(db.Integer, db.ForeingKey('massage.id'), nullable=False)
+    message = db.relationship('Message', backref=db.backref('tags', lazy=True))
+
+
+
 
 
 @app.route('/', methods=['GET', 'POST'])
