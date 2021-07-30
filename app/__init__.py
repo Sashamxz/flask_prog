2fc72b2f2ad3213315931579216c653ce386d11b
Sashamxz <sasha.oom7@gmail.com>
2021-07-30 08:26:17 +0300

    some bugs fix and refactor

--- app/__init__.py
+++ app/__init__.py
@@ -27,6 +27,11 @@ def create_app(config_name=None):
     # moment.init_app(app)
     db.init_app(app)
     
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
     return app
 
 
--- app/db.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from app import app
-from config import Configuration
-
-
-
-def init_db(app):
-    db = SQLAlchemy(app)
-    return db
