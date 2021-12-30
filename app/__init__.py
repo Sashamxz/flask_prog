2c8f6f593bc1ba7fde402071ebb9d83d40984ebd
Sashamxz <sasha.oom7@gmail.com>
2021-12-30 09:24:28 +0200

    flask secure

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,11 @@ from flask_security import Security
 from flask_security import current_user
 from config import  config
 
+
+
+
+
+
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 # mail = Mail()
@@ -20,29 +25,43 @@ migrate = Migrate()
 
 from app.models import Post, Role, Tag, User 
 
+
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 
 
-
-class AdminView(ModelView):
-    def is_accessible(self):
-        return current_user.has_role('admin')
-
-    def inaccessible_callback(self, name, **kwargs):
-        return redirect( url_for('security.login', next =request.url))       
-
-
-class HomeAdminView(AdminIndexView):
+class AdminMixin:
     def is_accessible(self):
         return current_user.has_role('admin')
 
     def inaccessible_callback(self, name,  **kwargs):
-        return redirect( url_for('security.login', next =request.url))       
+        return redirect( url_for('security.login', next =request.url))  
+
+
+class BaseModelView(ModelView):
+    def on_model_change(self, form, model, is_created):
+        
+        model.generate_slug()
+        return super(BaseModelView, self).on_model_change(form, model, is_created)
+
+
+class AdminView(AdminMixin, ModelView):
+    pass
+
+
+class HomeAdminView(AdminMixin, AdminIndexView):
+    pass         
+
+
+class PostAdminView(AdminMixin, BaseModelView):
+    form_columns = ['title',  'body', 'tags' ]
+
+
+class TagAdminView(AdminMixin, BaseModelView):
+    form_columns = ['name']
 
 
 
-def create_app(config_name=None):
-  
+def create_app(config_name=None): 
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
@@ -54,8 +73,8 @@ def create_app(config_name=None):
     db.init_app(app)
        
     admin = Admin(app, 'FlaskApp', url='/', index_view=HomeAdminView(name='Home'))
-    admin.add_view(AdminView(Post, db.session))
-    admin.add_view(AdminView(Tag, db.session))
+    admin.add_view(PostAdminView(Post, db.session))
+    admin.add_view(TagAdminView(Tag, db.session))
     admin.init_app
     migrate.init_app(app, db)
     #blueprint
